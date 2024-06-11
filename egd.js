2bf716ebdf7c5999ff0959a07a13e316246fad15
hi_mmmjie <60500606+365upupup@users.noreply.github.com>
2024-06-11 10:00:52 +0800

    Create egd.js

---
+++ egd.js
@@ -0,0 +1,7 @@
+[rewrite_local]
+
+^https:\/\/gateway\.ergediandian\.com\/dduser\/user\/center\/set url script-response-body https://raw.githubusercontent.com/365upupup/tesk/script/egdd.js
+
+[mitm]
+hostname = gateway.ergediandian.com
+*/
